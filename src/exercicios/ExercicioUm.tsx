9bd6b08d267d4ec7a7f838a422247b90d2c01498
vicente.collin@v4company.com <vicente.collin@v4company.com>
2026-04-23 14:32:47 -0300

    add: ex01

--- src/exercicios/ExercicioUm.tsx
+++ src/exercicios/ExercicioUm.tsx
@@ -1,11 +1,46 @@
 ﻿import ExercicioPlaceholder from "../components/ExercicioPlaceholder";
+import { useState } from "react";
 
 function ExercicioUm() {
+  const [contador, setContador] = useState(0)
+  const max = 10;
+  const min = 0;
+
+  function add(){
+    if (contador < max)
+      setContador(contador+1)
+  }
+
+  function sub(){
+    if (contador > min)
+      setContador(contador-1)
+  }
+ 
+
+  let mensagem = "Dentro do limite..."
+  if (contador == max)
+    mensagem = "Chegou ao limite!"
+  else if (contador == min)
+    mensagem = "O valor está muito baixo!"
+
   return (
-    <ExercicioPlaceholder
-      titulo="Exercicio 01 - Contador com Limites"
-      arquivoMarkdown="01-contador-com-limites.md"
-    />
+    <div className="grid grid-cols-1 justify-center "> 
+      <ExercicioPlaceholder
+        titulo="Exercicio 01 - Contador com Limites"
+        arquivoMarkdown="01-contador-com-limites.md"
+      />
+
+      <div className="grid grid-cols-1 text-center justify-center bg-white p-8 text-black">
+        <h1 id="contador" className="text-4xl font-bold m-2">{contador}</h1>
+        <p>{mensagem}</p>
+        <button className="bg-red-600 p-4 m-2" onClick={add}>+1</button>
+        <button className="bg-red-600 p-4 m-2" onClick={sub}>-1</button>
+        <button className="bg-red-600 p-4 m-2" onClick={()=>{setContador(0)}}>Remover</button>
+
+      </div>
+    </div>
+
+    
   );
 }
 
